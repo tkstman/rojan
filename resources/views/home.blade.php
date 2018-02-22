18e0652f9f04f7e4ac0bc6e5a819b9be09a405ab
Teran Stone <tstone@eoj.com.jm>
2018-02-22 16:58:43 -0500

    layout progress 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.master')
+
+@section('title')
+  Welcome!
+@endsection
+
+@section('content')
+<div>
+  @include('includes.carousel')
+</div>
+<div class="container">
+  <div class="row latest">
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInLeft">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+    <div class="col-md-3 wow slideInRight">
+        <div class="card mb-3 box-shadow">
+            <img class="card-img-top" data-src="" style="height: 225px; width:100%; display:block;"
+            src="https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg" data-holder-rendered="true">
+        </div>
+    </div>
+
+  </div>
+</div>
+@endsection
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="text-muted footer black-out mt-auto">
+    <div class="container">
+      <a href="#top" class="">
+        <div class="row footer-to-top">
+          <div class="mx-auto">Back To Top</div>
+        </div>
+      </a>
+      <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+      <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+    </div>
+</footer>
