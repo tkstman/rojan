36a1a4232122a25035c9ce231058e4670caa4f3d
Teran Stone <tstone@eoj.com.jm>
2018-03-13 13:40:16 -0500

    Quick Look Added

--- public/js/main.js
+++ public/js/main.js
@@ -10,15 +10,30 @@ $('.about_page').on('click', function(){
     $('#about_modal').modal();
 })
 
+/*
+  ADD QUICK LOOK BUTTON
+*/
 $('.card.mb-3.box-shadow').on('mouseenter', function()
 {
-  console.log("hi");
+  $(this).append('<span id="gw-quick-look-btn" class="a-button"> <span class="a-button-inner"> <input aria-hidden="true" class="a-button-input" type="submit" aria-labelledby="gw-quick-look-btn-announce"><span id="gw-quick-look-btn-announce" class="a-button-text" aria-hidden="true">Quick look</span></span></span>');
 })
 
+/*
+  REMOVE QUICK LOOK BUTTON
+*/
+$('.card.mb-3.box-shadow').on('mouseleave', function()
+{
+  $('#gw-quick-look-btn').remove();
+})
 
+/*
+  ANIMATE PANEL PAGES CLOSING AND SETUP FOR NEXT ANIMATION REQUEST
+*/
 $('.close').on('click', function(){
   //$('#contact_modal').removeClass('wow');
   $(this).parent().parent().parent().parent().removeClass("wow fadeIn animated");
   //$("#contact_modal")
   $(this).parent().parent().parent().parent().attr("style","visibility: visible; animation-name: fadeIn;");
 })
+
+//$('')
