0bbf5d4a9b2aaec7ad9235390e6316a1028c203d
Teran Stone <tstone@eoj.com.jm>
2018-04-12 16:54:07 -0500

    11-4-2018

--- app/Product.php
+++ app/Product.php
@@ -12,13 +12,13 @@ class Product extends Model
       return $this->belongsTo('App\User','audit_user','prod_id');
     }
 
-    // public function Image_Photo()
-    // {
-    //   return $this->belongsTo('App\Image_Photo','img_id','prod_id');
-    // }
-    //
-    // public function department()
-    // {
-    //   return $this->belongsTo('App\Department','dept_id','prod_id');
-    // }
+    public function Image_Photo()
+    {
+      return $this->belongsTo('App\Image_Photo','img_id','prod_id');
+    }
+
+    public function department()
+    {
+      return $this->belongsTo('App\Department','dept_id','prod_id');
+    }
 }
